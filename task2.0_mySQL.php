c7f14e41a8e2ca3967875e4959a980e7e5f34e6c
seppe <12001895@student.pxl.be>
2021-11-04 14:22:57 +0100

    Verbeteren lezen en schrijven naar mySQL met php

--- task2.0_mySQL.php
+++ task2.0_mySQL.php
@@ -10,15 +10,49 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_errno) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 
-$sql = "SELECT single, name_, age FROM student_12001895.Task";
-$result = $conn->query($sql) or die($conn->error);
+$sql = "SELECT id, name, age, adult FROM student_12001895.task_2";
+$result = $conn->query($sql); // or die($conn->error);
 
+
+echo '<body><table width = "600" border = "1" cellspacing = "0" cellpadding = "2">
+  <thead>
+    <tr>
+      <th>id: </th>
+      <th>Name: </th>
+      <th>Age: </th>
+      <th>18+: </th>
+    </tr>
+  </thead>';
 
   while($row = $result->fetch_assoc()) {
-    echo "name: " . $row["single"]. " - age: " . $row["name_"]. " - single: " . $row["age"]. "<br>";
+
+    $field1name = $row["id"];
+    $field2name = $row["name"];
+    $field3name = $row["age"];
+    $field4name = $row["adult"];
+
+    if($field4name == 1){
+      echo '<tr>
+            <td><b>'.$field1name.'</b></td> 
+            <td><b>'.$field2name.'</b></td> 
+            <td><b>'.$field3name.'</b></td> 
+            <td><b>'.$field4name.'</b></td> 
+            </tr>';      
+    }
+    else{
+      echo '<tr> 
+            <td>'.$field1name.'</td> 
+            <td>'.$field2name.'</td> 
+            <td>'.$field3name.'</td> 
+            <td>'.$field4name.'</td> 
+            </tr>';
+    }
   }
+
+  '</tbody>
+  </table></body>';
 
 $conn->close();
 ?>
--- task2.1_mySQL.php
+++ task2.1_mySQL.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $servername = "localhost";
 $username = "student_12001895";
 $password = "hgDgXCYb8OB1";
@@ -10,21 +12,26 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_errno) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully<br />";
 
 $name = $_POST['name'];
 $age = $_POST['age'];
 
+if(isset($_POST['submit']) && $_POST['randcheck']==$_SESSION['rand'])
+{
+    if(!empty($_POST['check'])){
+        $adult = 1;
+    }
+    else{
+        $adult = 0;
+    }
+    $sql = "INSERT INTO student_12001895.task_2".
+    "(name, age, adult) "."VALUES ".
+    "('$name','$age','$adult')";
 
-if(isset($_POST['submit'])){
-    $sql = "INSERT INTO student_12001895.Task ".
-    "(name, age) "."VALUES ".
-    "('$name','$age')";
-}
-
-
-if ($conn->query($sql)) {
-    printf("Record inserted successfully.<br />");
+    if ($conn->query($sql)) {
+        printf("Record inserted successfully.<br />");
+    }
 }
 
 if ($conn->errno) {
@@ -32,28 +39,34 @@ if ($conn->errno) {
     $conn->close();
 }
 else {
-?>  
-<form method = "post" action = "<?php $_PHP_SELF ?>">
-    <table width = "600" border = "0" cellspacing = "1" cellpadding = "2">
-        <tr>
-        <td width = "250">name</td>
-        <td><input name = "name" type = "text" id = "name"></td>
-        </tr>         
-        <tr>
-        <td width = "250">age</td>
-        <td><input name = "age" type = "text" id = "age"></td>
-        </tr>               
-        <tr>
-        <td width = "250"> </td>
-        <td></td>
-        </tr>         
-        <tr>
-        <td width = "250"> </td>
-        <td><input name = "submit" type = "submit" id = "submit"  value = "submit"></td>
-        </tr>
-    </table>
-</form>
-<?php
+    ?>  
+    <form method = "post" action = "">
+
+    <?php
+    $rand=rand();
+    $_SESSION['rand']=$rand;
+    ?>
+    <input type="hidden" value="<?php echo $rand; ?>" name="randcheck" />
+        <table width = "600" border = "0" cellspacing = "1" cellpadding = "2">
+            <tr>
+            <td width = "250">name</td>
+            <td><input name = "name" type = "text" id = "name"></td>
+            </tr>         
+            <tr>
+            <td width = "250">age</td>
+            <td><input name = "age" type = "text" id = "age"></td>
+            </tr>               
+            <tr>                    
+            <td width = "250">18+ </td>
+            <td  width = "250"><input type="checkbox"name="check"></td>
+            </tr>         
+            <tr>
+            <td width = "250"> </td>
+            <td><input name = "submit" type = "submit" id = "submit"  value = "submit"></td>
+            </tr>
+        </table>
+    </form>
+    <?php
 }
 
 $conn->close();
