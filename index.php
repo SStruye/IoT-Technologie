f16a7708459f97062657f5569c8f3b2a47205323
seppe <12001895@student.pxl.be>
2021-11-07 13:01:14 +0100

    working on layout

---
+++ index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Date Aquisition</title>
+</head>
+
+<body>
+    <div class = "topnav">
+        <li><a href="#home" class="active">home</a></li>
+        <li><a href="#contact" class="active">contact</a></li>
+        <li><a href="#about" class="active">about</a></li>
+    </div>
+
+    <div>
+        <p>123</p>
+        <p>123</p>
+        <p>123</p>
+        <p>123</p>
+        <p>123</p>
+        <p>123</p>
+        <p>123</p>
+    </div>
+
+</body>
+
+</html>
