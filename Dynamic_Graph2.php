f25fe5152d2de27d82688e5f1e09c6653c829fa3
seppe <12001895@student.pxl.be>
2021-12-27 17:10:34 +0100

    jqplot graph with both sensors on load

---
+++ Dynamic_Graph2.php
@@ -0,0 +1,25 @@
+
+
+<?php
+$servername = "localhost";
+$username = "student_12001895";
+$password = "hgDgXCYb8OB1";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_errno) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT value, timestamp FROM student_12001895.Sensor_Data where id='2' ";
+
+$container = array();
+if ($result = $conn->query($sql)) {
+while ($record = $result->fetch_assoc()) {
+            $container[] = array($record["timestamp"], intval($record["value"]));
+        }
+      }
+        $json_data = json_encode($container);
+echo $json_data;
+$conn->close();
+?>
